56955db5a7e99ce19428f07f186e0d84383213b2
zhenja-ulchak <super-zhenja@ukr.net>
2022-11-10 23:58:20 +0300

    modal_react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.scss';
+
+
+const Modal = ({open, setOpen, children}) => (
+  <div className={`overlay animated ${open ? 'show' : '' }`}>
+       <div className="modal">
+        <svg onClick={() => {setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
+           <title />
+           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+         </svg>
+{children}       </div>
+      </div> 
+)
 
 function App() {
+  const [open, setOpen] = React.useState(false)
+  // по умолчанію фалс
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* єслі клік то откриваєм модалку */}
+      <button onClick={() => {setOpen(true)}} className="open-modal-btn">✨ Открыть окно</button>
+       {/*  */}
+
+        <Modal open={open} setOpen={setOpen}>
+       
+                   <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+                   <p>open</p>
+        </Modal>
+       
+
+
+
+
+
+
+       {/* <div className={`overlay animated ${open ? 'show' : '' }`}>
+       <div className="modal">
+        {/* єслі фолс то закрити */}
+         {/* <svg onClick={() => {setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
+           <title />
+           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+         </svg>
+         <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+       </div> */}
+     {/* </div>  */}
+   
+
+{/* {open && (
+       <div className="overlay">
+       <div className="modal">
+        {/* єслі фолс то закрити */}
+         {/* <svg onClick={() => {setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
+           <title />
+           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+         </svg> */}
+         {/* <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+       </div>
+     </div> */}
+     {/* )} */}
     </div>
   );
 }
