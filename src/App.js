2261d5f59b9b933f81dd49605a8cd0ccc3763441
zhenja-ulchak <super-zhenja@ukr.net>
2022-11-13 17:34:10 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,64 +1,92 @@
-import React from 'react';
 import './index.scss';
+import React from 'react';
+
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
+
+function Result({correct}) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <a href='/'>
+      <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+   {/*  quest={quest} пропси */}
+function Game({step, quest, onclickVariant}) {
+
+  // Math.floor(Math.random() * 100) + 1;
+
+let addpx = Math.round( step /  questions.length *100 );
+console.log(step);
 
 
-const Modal = ({open, setOpen, children}) => (
-  <div className={`overlay animated ${open ? 'show' : '' }`}>
-       <div className="modal">
-          <svg onClick={() => {setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          {children}      
-        </div>
-      </div> 
-)
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${addpx}%` }} className="progress__inner"></div>
+      </div>
+      {/* рендерим з пропса {quest.title} */}
+      <h1> {quest.title}</h1>
+      <ul>
+       {
+        // передаєм в мап стрелочну функцію в неї в кладуєм значення і виводим в лі
+          quest.variants.map((text, index)=>(
+            //в функуію онклік передаєм індекс
+            <li onClick={()=> onclickVariant(index)} key={text}>{text}</li>
+          ))
+          }
+  
+      </ul>
+    </>
+  );
+}
 
 function App() {
-  const [open, setOpen] = React.useState(false)
-  // по умолчанію фалс
+  const [step, setStep] = new React.useState(0);
+  const quest = questions[step]; //із масива питань ми вибераєм по индексу step
+  const [correct, setCorrect] = new React.useState(0);
+
+
+ const onclickVariant = (index)=>{
+
+  setStep(step + 1);
+  if (index == quest.correct) {
+    setCorrect(correct + 1)
+  }
+
+ }
   return (
     <div className="App">
-      {/* єслі клік то откриваєм модалку */}
-      <button onClick={() => {setOpen(true)}} className="open-modal-btn">✨ Открыть окно</button>
-       {/*  */}
+      {/*  quest={quest} пропси */}
+      {
+        step !== questions.length ? (<Game step={step} quest={quest} onclickVariant={onclickVariant} />)
+         :
+         ( <Result correct={correct} /> )
+      }
 
-        <Modal open={open} setOpen={setOpen}>
-       
-                   <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-                   <p>open</p>
-        </Modal>
-       
-
-
-
-
-
-
-       {/* <div className={`overlay animated ${open ? 'show' : '' }`}>
-       <div className="modal">
-        {/* єслі фолс то закрити */}
-         {/* <svg onClick={() => {setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
-           <title />
-           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-         </svg>
-         <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-       </div> */}
-     {/* </div>  */}
-   
-
-{/* {open && (
-       <div className="overlay">
-       <div className="modal">
-        {/* єслі фолс то закрити */}
-         {/* <svg onClick={() => {setOpen(false)}} height="200" viewBox="0 0 200 200" width="200">
-           <title />
-           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-         </svg> */}
-         {/* <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-       </div>
-     </div> */}
-     {/* )} */}
     </div>
   );
 }
